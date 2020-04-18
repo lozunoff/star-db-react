8846ee4d4dcc201959a8df9d8c9019962b6a28da
lozunoff <andrey.lozunov@gmail.com>
2020-04-18 19:57:57 +0400

    Создан компонент "ErrorIndicator", который будет отображаться при перехвате ошибок

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './error-indicator.css';
+import icon from './death-star.png';
+
+const ErrorIndicator = () => (
+  <div className="error-indicator">
+    <img src={icon} alt="error icon" />
+    <span className="boom">BOOM!</span>
+    <span>something has gone terribly wrong</span>
+    <span>(but we already sent droids for fix it)</span>
+  </div>
+);
+
+export default ErrorIndicator;
